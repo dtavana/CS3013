72fd9f2b53a805bae92c6e31d18c2605c8209981
dtavana <dtavana@wpi.edu>
2020-10-08 13:06:38 -0400

    Finish project

--- Project4/proj4.cpp
+++ Project4/proj4.cpp
@@ -2,23 +2,65 @@
 using namespace std;
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 #include <string.h>
 #include <fcntl.h>
 #include <ctype.h>
 #define DEFAULTCHUNKSIZE 1024
 #define MAXCHUNKSIZE 8192
 
+int getPercentage(int num, int den)
+{
+    return ((float) num / den) * 100;
+}
+
 void startMmap(int fdIn)
 {
-    cout << "Starting mmap" << endl;
+    char* buffer;
+    struct stat sb;
+
+    if(fstat(fdIn, &sb) < 0) {
+        cerr << "Could not retrieve file size" << endl;
+        return;
+    }
+    if ((buffer = (char *) mmap (NULL, sb.st_size, PROT_READ, MAP_SHARED, fdIn, 0)) == (char*) -1) {
+        cerr << "Could not mmap file" << endl;
+        return;
+    }
+
+    int printable = 0, totalBytes = sb.st_size;
+    for(int i = 0; i < sb.st_size; i++) {
+        if(isprint(buffer[i]) || isspace(buffer[i])) {
+            printable++;
+        }
+    }
+    int percentage = getPercentage(printable, totalBytes);
+    cout << printable << " printable characters out of " << totalBytes << " bytes, " << percentage << "% using mmap()" << endl;
+
+    if(munmap(buffer, sb.st_size) < 0) {
+        cerr << "Could not unmap memory" << endl;
+        return;
+    }
 }
 
 void startRead(int chunkSize, int fdIn)
 {
-    cout << "Starting read" << endl;
+    char buffer[chunkSize];
+    int bytesRead, printable = 0, totalBytes = 0;
+    while((bytesRead = read(fdIn, buffer, chunkSize)) > 0) {
+        for(int i = 0; i < bytesRead; i++) {
+            if(isprint(buffer[i]) || isspace(buffer[i])) {
+                printable++;
+            }
+        }
+        totalBytes += bytesRead;
+    }
+    int percentage = getPercentage(printable, totalBytes);
+    cout << printable << " printable characters out of " << totalBytes << " bytes, " << percentage << "% using read()" << endl;
 }
 
 int main(int argc, char* argv[])
@@ -40,7 +82,7 @@ int main(int argc, char* argv[])
     }
 
     int fdIn;
-    if(fdIn = open(file.c_str(), O_RDONLY) < 0) {
+    if((fdIn = open(file.c_str(), O_RDONLY)) < 0) {
         cerr << "Failed to open " << file << endl;
         exit(1);
     }
@@ -50,4 +92,5 @@ int main(int argc, char* argv[])
     else {
         startRead(chunkSize, fdIn);
     }
+    close(fdIn);
 }
